98f1564d2747e98c2855e805f9000179e9953e61
Kaan Arslan <oguzkaanarslan@outlook.com>
2022-03-07 18:37:40 +0300

    created context

---
+++ src/components/Button.js
@@ -0,0 +1,13 @@
+import React, { useContext } from 'react';
+import ThemeContext from "../context/ThemeContext";
+
+
+function Button() {
+    const data = useContext(ThemeContext);
+    console.log(data);
+  return (
+    <div>Button</div>
+  )
+}
+
+export default Button
